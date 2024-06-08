3f0765c03be6c23e94435ba28f51e5c0900cbe23
DaniilD26 <daniil.d2601@gmail.com>
2024-06-08 14:01:32 +0500

    asset paths2 img

--- resources/views/menu/show.blade.php
+++ resources/views/menu/show.blade.php
@@ -5,6 +5,9 @@
     
         <div class="card" style="width: 30rem;">
             <img src="{{  '/storage/app/'.$menu->card }}" class="card-img-top" alt="Изображение товара">
+            <img src="{{  asset($menu->card)}}" class="card-img-top" alt="Изображение товара">
+            <img src="{{  '/storage/app/'.$menu->card }}" class="card-img-top" alt="Изображение товара">
+            
             {{-- <img src="\storage\app\public\images\vlTcrvdej7FZ5YllolqCOHgNZiyVHZYW8Ar3jN5o.webp" class="card-img-top" alt="Изображение товара"> --}}
 
             <div class="card-body">
