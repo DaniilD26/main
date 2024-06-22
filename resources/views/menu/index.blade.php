c0699f0d62fc1e288152a4f8908190a225c5cef6
DaniilD26 <daniil.d2601@gmail.com>
2024-06-22 17:57:56 +0500

    fix delete5

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -18,7 +18,15 @@
                     <td>{{$menu->description}}</td>
                     <td>{{$menu->price}} руб.</td>
                     <td><a href="{{route('menu.show', $menu)}}">Подробнее</a></td>
-                    <td><a href="{{route('menu.destroy', $menu->id)}}">Удалить</a></td>
+                    <td>
+                        <a href="{{route('menu.destroy', $menu->id)}}">Удалить</a>
+                        <form method="POST" action="{{route('menu.destroy', $menu->id)}}"  enctype="multipart/form-data">
+                            @csrf {{-- Встраиваем CSRF-токен --}}
+                            @method('DELETE') {{-- Указываем метод DELETE --}}
+                            <button type="submit">Удалить блюдо</button>
+                        </form>
+                    
+                    </td>
                 </tr>
             @endforeach
         </table>
