ecfd52cf702a729d623fcf3afd4f5d6bc8913441
DaniilD26 <daniil.d2601@gmail.com>
2024-06-23 14:18:33 +0500

    panel2

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,22 +6,22 @@
 <h1>Административная панель</h1>
 <h3>Привет, {{ Auth::user()->login }}!</h3>
 <div class="card" style="width: 30rem;">
-    <img src="{{ asset('storage/'.$user->avatar) }}" class="card-img-top" alt="Картинка">
+    <img src="{{ asset('storage/'.Auth::user()->avatar) }}" class="card-img-top" alt="Картинка">
 
     <div class="card-body">
         <h5 class="card-title">Профиль</h5>
     </div>
     <ul class="list-group list-group-flush">
-        <li class="list-group-item">Логин: <b>{{ $user->login }}</b></li>           
-        <li class="list-group-item">Имя: <b>{{ $user->name }}</b></li>
-        <li class="list-group-item">Фамилия: <b>{{ $user->lastname }}</b></li>
-        <li class="list-group-item">Телефон: <b>{{ $user->phone }}</b></li>
-        <li class="list-group-item">Возраст: <b>{{ $user->age }}</b></li>
-        <li class="list-group-item">Роль: <b>{{ $user->role->role }}</b></li>
+        <li class="list-group-item">Логин: <b>{{ Auth::user()->login }}</b></li>           
+        <li class="list-group-item">Имя: <b>{{ Auth::user()->name }}</b></li>
+        <li class="list-group-item">Фамилия: <b>{{ Auth::user()->lastname }}</b></li>
+        <li class="list-group-item">Телефон: <b>{{ Auth::user()->phone }}</b></li>
+        <li class="list-group-item">Возраст: <b>{{ Auth::user()->age }}</b></li>
+        <li class="list-group-item">Роль: <b>{{ Auth::user()->role->role }}</b></li>
         <li class="list-group-item">Статус работника: <b>{{ $user->status->status }}</b></li>
 </ul>
          <div class="card-body">
-           <a href="{{route('users.edit', $user->id)}}" class="card-link">Редактировать данные</a>
+           <a href="{{route('users.edit', Auth::user()->id)}}" class="card-link">Редактировать данные</a>
             </div>
 </div>
 </div>
